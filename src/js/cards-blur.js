2a19c1477a6b4f18d1e48d39b59a84d04f0c3ac9
ivanlomovtsev <i.d.lomovtsev@gmail.ru>
2023-01-25 22:48:34 +0500

    feat: add blur filter for cards

--- src/js/cards-blur.js
+++ src/js/cards-blur.js
@@ -6,33 +6,61 @@ const gardenCard = document.getElementsByClassName("gardens");
 const lawnCard = document.getElementsByClassName("lawn");
 const plantingCard = document.getElementsByClassName("planting");
 
-gardenBtn.addEventListener('click', controlBlur.bind({button: gardenBtn, firstCardBlur: lawnCard, secondCardBlur: plantingCard}));
-lawnBtn.addEventListener('click', controlBlur.bind({button: lawnBtn, firstCardBlur: gardenCard, secondCardBlur: plantingCard}));
-plantingBtn.addEventListener('click', controlBlur.bind({button: plantingBtn, firstCardBlur: gardenCard, secondCardBlur: lawnCard}));
+gardenBtn.addEventListener('click', controlBlur.bind({
+    button: gardenBtn, 
+    cardUnBlur: gardenCard, 
+    firstCardBlur: lawnCard, 
+    secondCardBlur: plantingCard
+}));
+lawnBtn.addEventListener('click', controlBlur.bind({
+    button: lawnBtn,  
+    cardUnBlur:lawnCard, 
+    firstCardBlur: gardenCard, 
+    secondCardBlur: plantingCard
+}));
+plantingBtn.addEventListener('click', controlBlur.bind({
+    button: plantingBtn, 
+    cardUnBlur: plantingCard, 
+    firstCardBlur: gardenCard, 
+    secondCardBlur: lawnCard
+}));
 
 let queueButton = [];
-
+let unBlur = [];
 
 function controlBlur() {
     if (queueButton.indexOf(this.button) !== -1) {
         (this.button).classList.remove('button-active');
         queueButton.splice(queueButton.indexOf(this.button), 1);
+        unBlur.splice(unBlur.indexOf(this.cardUnBlur), 1);
+        console.log(unBlur)
+        addBlur(unBlur)
         return
     }
     if (queueButton.length < 2) {
         queueButton.push(this.button);
-        (this.button).classList.add('button-active')
+        unBlur.push(this.cardUnBlur);
+        (this.button).classList.add('button-active');
     }
     else {
         queueButton.push(this.button);
         (this.button).classList.toggle('button-active');
+        unBlur.push(this.cardUnBlur);
+        unBlur.shift();
         (queueButton.shift()).classList.toggle('button-active');
     }
+    addBlur(unBlur)
 }
-    
 
-    // console.log(queueButton);
-
-    // (this.button).classList.toggle('button-active');
-    // Array.from(this.firstCardBlur).forEach(card => card.classList.toggle('blur'));
-    // Array.from(this.secondCardBlur).forEach(card => card.classList.toggle('blur'));
+function addBlur (unBlurCards) {
+    let allCards = [gardenCard, lawnCard, plantingCard];
+    if (unBlurCards.length === 0){
+         allCards.forEach(cardsOneStyle => Array.from(cardsOneStyle).forEach(card=>card.style.filter = 'none'))
+         return
+    }
+    let blurCards = allCards
+                            .filter(card => !Array.from(unBlurCards).includes(card))
+                            .concat(Array.from(unBlurCards).filter(card => !allCards.includes(card)))
+    unBlurCards.forEach(cardsOneStyle => Array.from(cardsOneStyle).forEach(card=>card.style.filter = 'none'))
+    blurCards.forEach(cardsOneStyle => Array.from(cardsOneStyle).forEach(card=>card.style.filter = 'blur(2px)'))
+}
