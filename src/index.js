b33d1330732fe2ab557a2daeb3cde13e87b4bdea
ivanlomovtsev <i.d.lomovtsev@gmail.ru>
2023-01-30 22:05:04 +0500

    feat: add accordion in service

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './styles/main.scss';
 import './js/menu.js'
 import './js/cards-blur.js'
+import './js/accordion.js'
---
+++ src/js/accordion.js
@@ -0,0 +1,25 @@
+document.querySelectorAll(".accordion__button").forEach((el)=>{
+    el.addEventListener('click', ()=>{
+        let content = el.nextElementSibling;
+        if (content.style.maxHeight) {
+            document.querySelectorAll('.accordion__content').forEach((el) => {
+                el.style.maxHeight = null;
+                el.style.marginBottom = "20px";
+            });
+            el.classList.remove("accordion__button_active");
+        }
+        else {
+            document.querySelectorAll('.accordion__content').forEach((el)=> {
+                el.style.maxHeight = null
+            });
+            document.querySelectorAll('.accordion__button').forEach((el) =>{
+                if(el.classList.contains('accordion__button_active')) {
+                    el.classList.remove("accordion__button_active");
+                }
+            })
+            content.style.maxHeight = content.scrollHeight + 'px';
+            content.style.marginBottom = "0px";
+            el.classList.add("accordion__button_active");
+        }
+    })
+})
