25df7d97b9734f2788b1e49fa165ec31402065c6
ivanlomovtsev <i.d.lomovtsev@gmail.ru>
2023-02-04 22:57:08 +0500

    feat: add select in contacts

--- src/js/select.js
+++ src/js/select.js
@@ -1,12 +1,38 @@
+const dropDownButton = document.querySelector(".dropdown__button");
+const dropDownButtonSpan = document.querySelector(".dropdown__button-span")
+const dropDownList = document.querySelector(".dropdown__list");
+const dropDownListItems = document.querySelectorAll(".dropdown__list-item")
+const dropDownInput = document.querySelector(".dropdown__input-hidden")
 
 //клик по кнопке. Открыть/закрыть select
-document.querySelector(".dropdown__button").addEventListener('click', function () {
-    document.querySelector(".dropdown__list").classList.toggle('dropdown__list--visible')
+dropDownButton.addEventListener('click', function () {
+    dropDownList.classList.toggle('dropdown__list--visible')
+    dropDownButton.classList.toggle('dropdown__button-active')
 })
 
 //Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
-document.querySelectorAll(".dropdown__list-item").forEach(function(listItem) {
+dropDownListItems.forEach(function(listItem) {
     listItem.addEventListener('click', function() {
-        document.querySelector(".dropdown__button").innerText = this.innerText;
+        dropDownButtonSpan.innerText = this.innerText;
+        dropDownButtonSpan.style.fontSize = "16px";
+        dropDownButton.style.backgroundColor = "#C1E698";
+        dropDownInput.value = this.dataset.value;
+        dropDownList.classList.remove('dropdown__list--visible')
+        dropDownButton.classList.remove('dropdown__button-active')
     })
+})
+
+//клик снаружи дропдауна. Закрываем дропдаун
+document.addEventListener('click', function(e){
+    if (!e.composedPath().includes(dropDownButton)) {
+         dropDownList.classList.remove('dropdown__list--visible')
+         dropDownButton.classList.remove('dropdown__button-active')
+    }
+
+})
+document.addEventListener('keydown', function(e){
+    if (e.key === "Tab" || e.key === "Escape") {
+        dropDownList.classList.remove('dropdown__list--visible')
+        dropDownButton.classList.remove('dropdown__button-active')
+    }
 })
